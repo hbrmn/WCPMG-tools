3e69634dff8e9981b250f0fa32726be8e80ac11c
Henrik Bradtmüller <67208403+hbrmn@users.noreply.github.com>
2022-10-27 16:34:17 -0300

    Code file for debugging

--- WCPMG-Splitadd_debug.py
+++ WCPMG-Splitadd_debug.py
@@ -19,8 +19,9 @@ number_of_echoes_to_add = 1
 first_echo = 1
 vendor = 'varian'
 export_data = 'y'
+compare_with_spikelett = 1
 zero_fill = 18 * 1024
-normalize_spectrum = 'y'
+debug = 0
 ##### Built-in functions
 
 ##### Export the spectrum in DMFit format for easy use in Origin and ssNake
@@ -44,6 +45,7 @@ if vendor == 'bruker':
 
     spectrometer_frequency = dic['procs']['SF']
 
+    # num_echoes = dic['acqus']['L'][22] - not used
     pulse_length = dic['acqus']['P'][1]
     echo_length = dic['acqus']['D'][6] * 1e6
     blank_length = dic['acqus']['D'][3] * 1e6
@@ -67,6 +69,7 @@ if vendor == 'bruker':
 
 elif vendor == 'varian':
     dic, rawdata = ng.varian.read(path)
+    # rawdata = rawdata[0]
 
     spectrometer_frequency = float(dic['procpar']['sfrq']['values'][0])
 
@@ -90,12 +93,14 @@ elif vendor == 'varian':
     post_pulse_blank = float(dic['procpar']['r3Xcpmg']['values'][0])
     loop_length = float(dic['procpar']['tauXcpmg']['values'][0])
 
+
     blank_length = (pulse_length + pre_pulse_blank + post_pulse_blank +
-                    rof2 + 0.1) + 3.3 # 3.3 unknown
+                    rof2 + 0.1) + 3.3 # 3.3unknown
 
     echo_length = loop_length - (pulse_length + pre_pulse_blank +
                                  post_pulse_blank + rof2  + 0.1) # 0.1 unknown
     echo_length = rof1 + echo_length + rof3 + rof3 # 0.6 is found
+    # echo_length = 92.4
 
     spectral_width = float(dic['procpar']['sw']['values'][0])
 
@@ -114,6 +119,17 @@ elif vendor == 'varian':
                         blank_block)
     data = ng.process.proc_base.roll(data, pts=zero_fill_length, neg=False)
 
+# Cut off first echo - find first 0 start there with chopping up
+# then take first echo, fill in the beginning with zeroes to correspond to same
+# length and add to others
+
+
+##### Phasing of the FID for debugging
+# data = ng.proc_autophase.autops(rawdata, "acme", p0=0, p1=0)
+# data = np.abs(rawdata)
+
+##### Initialize an empty figure
+figure = fig = plt.figure()
 
 ##### Initialize fid variable to store the added FIDs
 fid = np.array([0])
@@ -124,17 +140,36 @@ for i in range(number_of_echoes_to_add):
     echo_start = (int(blank_block * (i+first_echo)
                       + echo_length_points * (i+first_echo-1)))
     echo_end = int(echo_start + echo_length_points)
-
+    if debug == 1:
+        # Plot all individual FIDs for debugging
+        test = data[echo_start:echo_end]
+        plt.plot(data[echo_start:echo_end])
     fid = fid + data[echo_start:echo_end]
+
+##### Approach FFT and subsequent addition - yields same result
+# for i in range(number_of_echoes_to_add):
+
+#     echo_start = (int(blank_block * (i+first_echo)
+#                       + echo_length_points * (i+first_echo-1)))
+#     echo_end = int(echo_start + echo_length_points)
+#     if debug == 1:
+#         # Plot all individual FIDs for debugging
+#         test = data[echo_start:echo_end]
+#         plt.plot(data[echo_start:echo_end])
+#     fid = data[echo_start:echo_end]
+#     fid = ng.proc_base.zf_size(fid, zero_fill)
+#     spec = spec + ng.proc_base.fft(fid)
 
 ##### Zero filling and Fourier transformation
 fid = ng.proc_base.zf_size(fid, zero_fill)
 spec = ng.proc_base.fft(fid)
 
 ##### Creates ppm axis with correct referencing for Bruker data
+
 if vendor == 'bruker':
     spec = ng.proc_base.rev(spec)
 length = spec.shape[data.ndim - 1]
+
 
 freq_scale = (
     (np.arange((spectral_width/2) + relative_offset_frequency,
@@ -147,27 +182,27 @@ ppm_scale = freq_scale/carrier_freq
 # Transmitter offset frequency in points
 # Selecting data size out of array
 sfo_point = (int(np.round(length/2)+ (relative_offset_frequency /
-                                      (spectral_width/length))))
+                                      (spectral_width/length)
+                         )))
 
+if compare_with_spikelett == 1:
+    ##### Show spikelett spectrum for debugging
+    spec_compare = ng.proc_base.zf_size(rawdata, zero_fill)
+    spec_compare = ng.proc_base.fft(spec_compare)
+    if vendor == 'bruker':
+        spec_compare = ng.proc_base.rev(spec_compare)
 
-##### Show spikelett spectrum for debugging
-spec_compare = ng.proc_base.zf_size(rawdata, zero_fill)
-spec_compare = ng.proc_base.fft(spec_compare)
-
-if vendor == 'bruker':
-    spec_compare = ng.proc_base.rev(spec_compare)
-
-if normalize_spectrum == 'y':
     spec_compare = np.abs(spec_compare)
     spec_compare = spec_compare/np.max(spec_compare)
-    spec = np.abs(spec)
-    spec = spec/np.max(spec)
+    plt.plot(ppm_scale, spec_compare)
 
+spec = np.abs(spec)
+spec = spec/np.max(spec)
 ##### Plot spectrum from added FIDs
-figure = fig = plt.figure()
-plt.plot(ppm_scale, spec_compare)
-plt.plot(ppm_scale, spec)
-plt.gca().invert_xaxis()
+if debug != 1:
+    plt.plot(ppm_scale, spec)
+    plt.gca().invert_xaxis()
+
 
 if export_data == 'y':
     export(freq_scale, abs(spec), '##freq',
